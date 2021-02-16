7c85c0c4ed07bcda81d50d50b84c91c57eacd653
Moath <moath.alhajrii@gmail.com>
2021-02-16 15:23:24 +0300

    add prettier and fix code style

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,31 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jwt-simple');
 
-
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', function (req, res) {
+    res.render('index');
 });
 
-router.get('/about', function(req, res) {
-  res.render('about');
+router.get('/about', function (req, res) {
+    res.render('about');
 });
 
-router.post('/tokens', function(req, res) {
-
+router.post('/tokens', function (req, res) {
     var claims = req.body.claims,
         key = req.body.key,
         alg = req.body.alg;
 
-    if(!claims) {
-        res.send(400, "Missing claims");
+    if (!claims) {
+        res.send(400, 'Missing claims');
         return;
     }
 
-    if(!key) {
-        res.send(400, "Missing key");
+    if (!key) {
+        res.send(400, 'Missing key');
         return;
     }
 
-    if(!alg) {
-        res.send(400, "Missing alg");
+    if (!alg) {
+        res.send(400, 'Missing alg');
         return;
     }
 
@@ -39,13 +37,12 @@ router.post('/tokens', function(req, res) {
     try {
         var token = jwt.encode(claims, key, alg);
         var response = {
-            token:token
+            token: token,
         };
 
         res.send(200, response);
-    }
-    catch(ex) {
-        var msg = "Error creating token: " + ex;
+    } catch (ex) {
+        var msg = 'Error creating token: ' + ex;
         res.send(400, msg);
     }
 });
