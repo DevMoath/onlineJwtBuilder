589a7942ae55e763af128034f337aeff1cdb90cf
Moath <moath.alhajrii@gmail.com>
2021-06-18 16:29:01 +0300

    replace www with index.js and use heroku

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-
-var app = express();
+const express = require('express');
+const path = require('path');
+const favicon = require('static-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +22,7 @@ app.use('/', routes);
 
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
---
+++ index.js
@@ -0,0 +1,8 @@
+const debug = require('debug')('onlineJwtBuilder');
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), function () {
+    debug('Express server listening on port ' + server.address().port);
+});
