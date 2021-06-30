9dc2c7af3124135d487ad8b1216d0a7450890fb0
Moath <moath.alhajrii@gmail.com>
2021-07-01 02:32:52 +0300

    combine base64.js with index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
-function addMinutes(date, minutes) {
+function addMinutes (date, minutes) {
     return new Date(date.getTime() + minutes * 60000);
 }
 
-Array.prototype.clear = function () {
+Array.prototype.clear = function() {
     while (this.length > 0) {
         this.pop();
     }
 };
 
-function createKey(charCount) {
+function createKey (charCount) {
     let key = '';
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -19,7 +19,7 @@ function createKey(charCount) {
     return key;
 }
 
-var masterViewModel = function (standardClaims, additionalClaims) {
+var masterViewModel = function(standardClaims, additionalClaims) {
     var self = this;
 
     self.standardClaims = standardClaims;
@@ -33,11 +33,11 @@ var masterViewModel = function (standardClaims, additionalClaims) {
     self.selectedAlgorithm = ko.observable('HS256');
     self.algorithms = ko.observableArray(['HS256', 'HS384', 'HS512']);
 
-    self.keyLength = ko.computed(function () {
+    self.keyLength = ko.computed(function() {
         return self.key().length;
     });
 
-    self.generatedClaimSet = ko.computed(function () {
+    self.generatedClaimSet = ko.computed(function() {
         var iat = new Date(this.standardClaims.issuedAt()).getTime();
         var exp = new Date(this.standardClaims.expiration()).getTime();
         var sub = this.standardClaims.subject();
@@ -80,11 +80,11 @@ var masterViewModel = function (standardClaims, additionalClaims) {
         return claimSet;
     }, self);
 
-    self.generatedClaimSetDisplay = ko.computed(function () {
+    self.generatedClaimSetDisplay = ko.computed(function() {
         return JSON.stringify(this.generatedClaimSet(), null, 4);
     }, this);
 
-    self.clearCreatedJwt = ko.computed(function () {
+    self.clearCreatedJwt = ko.computed(function() {
         self.generatedClaimSet();
         self.key();
         self.createdJwt('');
@@ -92,78 +92,78 @@ var masterViewModel = function (standardClaims, additionalClaims) {
         self.selectedAlgorithm();
     }, self);
 
-    self.toggleBase64 = function () {
+    self.toggleBase64 = function() {
         var current = self.isBase64Encoding();
         self.isBase64Encoding(!current);
         return false;
     };
 
-    self.toggleAlgorithm = function (item) {
+    self.toggleAlgorithm = function(item) {
         self.selectedAlgorithm(item);
     };
 
-    self.issuedAtSetNow = function () {
+    self.issuedAtSetNow = function() {
         this.standardClaims.issuedAt(new Date().toISOString());
     };
 
-    self.expirationSetNow = function () {
+    self.expirationSetNow = function() {
         this.standardClaims.expiration(new Date().toISOString());
     };
 
-    self.expirationSetTwentyMinutes = function () {
+    self.expirationSetTwentyMinutes = function() {
         var now = new Date();
         var later = addMinutes(now, 20);
         this.standardClaims.expiration(later.toISOString());
     };
 
-    self.expirationSetOneYear = function () {
+    self.expirationSetOneYear = function() {
         var now = new Date();
         var later = addMinutes(now, 365 * 24 * 60);
         this.standardClaims.expiration(later.toISOString());
     };
 
-    self.clearAllAdditionalClaims = function () {
+    self.clearAllAdditionalClaims = function() {
         while (this.additionalClaims().length > 0) {
             this.additionalClaims.pop();
         }
     };
 
-    self.addOneAdditionalClaim = function () {
+    self.addOneAdditionalClaim = function() {
         this.additionalClaims.push(new claimViewModel('', ''));
     };
 
-    self.addEmailAdditionalClaim = function () {
+    self.addEmailAdditionalClaim = function() {
         this.additionalClaims.push(new claimViewModel('Email', 'bee@example.com'));
     };
 
-    self.addNameNetAdditionalClaim = function () {
+    self.addNameNetAdditionalClaim = function() {
         this.additionalClaims.push(
-            new claimViewModel('http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name', 'jrocket')
+            new claimViewModel('http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name', 'jrocket'),
         );
     };
 
-    self.addRoleNetAdditionalClaim = function () {
+    self.addRoleNetAdditionalClaim = function() {
         this.additionalClaims.push(
-            new claimViewModel('http://schemas.microsoft.com/ws/2008/06/identity/claims/role', 'Manager')
+            new claimViewModel('http://schemas.microsoft.com/ws/2008/06/identity/claims/role', 'Manager'),
         );
     };
 
-    self.addEmailNetAdditionalClaim = function () {
+    self.addEmailNetAdditionalClaim = function() {
         this.additionalClaims.push(
-            new claimViewModel('http://schemas.xmlsoap.org/ws/2005/05/identity/claims/email', 'bee@example.com')
+            new claimViewModel('http://schemas.xmlsoap.org/ws/2005/05/identity/claims/email', 'bee@example.com'),
         );
     };
 
-    self.removeClaim = function (claim) {
+    self.removeClaim = function(claim) {
         self.additionalClaims.remove(claim);
     };
 
-    self.generateSymmetricKey = function (charCount) {
+    self.generateSymmetricKey = function(charCount) {
         self.key(createKey(charCount));
         return false;
     };
 
-    self.createJwt = function () {
+    self.createJwt = function() {
         var request = {
             claims: this.generatedClaimSet(),
             key: this.key(),
@@ -182,7 +182,7 @@ var masterViewModel = function (standardClaims, additionalClaims) {
         });
     };
 
-    self.onTokenSuccess = function (data, status) {
+    self.onTokenSuccess = function(data, status) {
         var token = data.token;
         if (self.isBase64Encoding()) {
             token = base64.encode(token);
@@ -191,26 +191,26 @@ var masterViewModel = function (standardClaims, additionalClaims) {
         self.createdJwt(token);
     };
 
-    self.onTokenError = function (error) {
+    self.onTokenError = function(error) {
         self.createdJwt(error.responseText);
     };
 
-    self.noop = function () {};
+    self.noop = function() {};
 
-    self.warnings = ko.computed(function () {
+    self.warnings = ko.computed(function() {
         var warnings = [];
 
         var dt = new Date(this.standardClaims.issuedAt());
         if (isNaN(dt)) {
             warnings.push(
-                'IssuedAt is not a valid <a href="http://www.w3.org/TR/NOTE-datetime">W3C date/time</a>. Must be formatted as: YYYY-MM-DDThh:mm:ssZ'
+                'IssuedAt is not a valid <a href="http://www.w3.org/TR/NOTE-datetime">W3C date/time</a>. Must be formatted as: YYYY-MM-DDThh:mm:ssZ',
             );
         }
 
         dt = new Date(this.standardClaims.expiration());
         if (isNaN(dt)) {
             warnings.push(
-                'Expiration  is not a valid <a href="http://www.w3.org/TR/NOTE-datetime">W3C date/time</a>. Must be formatted as: YYYY-MM-DDThh:mm:ssZ'
+                'Expiration  is not a valid <a href="http://www.w3.org/TR/NOTE-datetime">W3C date/time</a>. Must be formatted as: YYYY-MM-DDThh:mm:ssZ',
             );
         }
 
@@ -218,7 +218,7 @@ var masterViewModel = function (standardClaims, additionalClaims) {
     }, self);
 };
 
-var standardClaimsViewModel = function (issuer, issuedAt, expiration, audience, subject) {
+var standardClaimsViewModel = function(issuer, issuedAt, expiration, audience, subject) {
     var self = this;
 
     self.issuer = ko.observable(issuer);
@@ -228,13 +228,13 @@ var standardClaimsViewModel = function (issuer, issuedAt, expiration, audience, 
     self.subject = ko.observable(subject);
 };
 
-var createMaster = function () {
+var createMaster = function() {
     var standardClaims = new standardClaimsViewModel(
         'Online JWT Builder',
         '2014-07-14T08:30Z',
         '2014-07-16T19:20Z',
         'www.example.com',
-        'jrocket@example.com'
+        'jrocket@example.com',
     );
 
     var additionalClaims = [
@@ -250,18 +250,18 @@ var createMaster = function () {
     return master;
 };
 
-var claimViewModel = function (claimType, value) {
+var claimViewModel = function(claimType, value) {
     var self = this;
 
     self.claimType = ko.observable(claimType);
     self.value = ko.observable(value);
 };
 
-window.onload = function () {
-    $(function () {
+window.onload = function() {
+    $(function() {
         const copyButton = document.getElementById('copy-button');
 
-        copyButton.addEventListener('click', function () {
+        copyButton.addEventListener('click', function() {
             const createdJwt = document.getElementById('created-jwt');
             const copyText = document.getElementById('hidden_input');
 
@@ -276,7 +276,7 @@ window.onload = function () {
 
             copyButton.innerText = 'Copied';
 
-            setTimeout(function () {
+            setTimeout(function() {
                 copyButton.innerText = 'Copy JWT to Clipboard';
             }, 3000);
         });
@@ -288,11 +288,164 @@ window.onload = function () {
         viewModel.issuedAtSetNow();
         viewModel.expirationSetOneYear();
 
-        $('a').on('click', function (event) {
+        $('a').on('click', function(event) {
             const href = event.currentTarget.href;
             if (href.indexOf('#') === href.length - 1) {
                 event.preventDefault();
             }
         });
     });
+};
+
+/*
+ * Copyright (c) 2010 Nick Galbreath
+ * http://code.google.com/p/stringencoders/source/browse/#svn/trunk/javascript
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* base64 encode/decode compatible with window.btoa/atob
+ *
+ * window.atob/btoa is a Firefox extension to convert binary data (the "b")
+ * to base64 (ascii, the "a").
+ *
+ * It is also found in Safari and Chrome.  It is not available in IE.
+ *
+ * if (!window.btoa) window.btoa = base64.encode
+ * if (!window.atob) window.atob = base64.decode
+ *
+ * The original spec's for atob/btoa are a bit lacking
+ * https://developer.mozilla.org/en/DOM/window.atob
+ * https://developer.mozilla.org/en/DOM/window.btoa
+ *
+ * window.btoa and base64.encode takes a string where charCodeAt is [0,255]
+ * If any character is not [0,255], then an exception is thrown.
+ *
+ * window.atob and base64.decode take a base64-encoded string
+ * If the input length is not a multiple of 4, or contains invalid characters
+ *   then an exception is thrown.
+ */
+base64 = {};
+base64.PADCHAR = '=';
+base64.ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+base64.getbyte64 = function(s, i) {
+    // This is oddly fast, except on Chrome/V8.
+    //  Minimal or no improvement in performance by using a
+    //   object with properties mapping chars to value (eg. 'A': 0)
+    var idx = base64.ALPHA.indexOf(s.charAt(i));
+    if (idx == -1) {
+        throw 'Cannot decode base64';
+    }
+    return idx;
+};
+
+base64.decode = function(s) {
+    // convert to string
+    s = '' + s;
+    var getbyte64 = base64.getbyte64;
+    var pads, i, b10;
+    var imax = s.length;
+    if (imax == 0) {
+        return s;
+    }
+
+    if (imax % 4 != 0) {
+        throw 'Cannot decode base64';
+    }
+
+    pads = 0;
+    if (s.charAt(imax - 1) == base64.PADCHAR) {
+        pads = 1;
+        if (s.charAt(imax - 2) == base64.PADCHAR) {
+            pads = 2;
+        }
+        // either way, we want to ignore this last block
+        imax -= 4;
+    }
+
+    var x = [];
+    for (i = 0; i < imax; i += 4) {
+        b10 = (getbyte64(s, i) << 18) | (getbyte64(s, i + 1) << 12) | (getbyte64(s, i + 2) << 6) | getbyte64(s, i + 3);
+        x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff, b10 & 0xff));
+    }
+
+    switch (pads) {
+        case 1:
+            b10 = (getbyte64(s, i) << 18) | (getbyte64(s, i + 1) << 12) | (getbyte64(s, i + 2) << 6);
+            x.push(String.fromCharCode(b10 >> 16, (b10 >> 8) & 0xff));
+            break;
+        case 2:
+            b10 = (getbyte64(s, i) << 18) | (getbyte64(s, i + 1) << 12);
+            x.push(String.fromCharCode(b10 >> 16));
+            break;
+    }
+    return x.join('');
+};
+
+base64.getbyte = function(s, i) {
+    var x = s.charCodeAt(i);
+    if (x > 255) {
+        throw 'INVALID_CHARACTER_ERR: DOM Exception 5';
+    }
+    return x;
+};
+
+base64.encode = function(s) {
+    if (arguments.length != 1) {
+        throw 'SyntaxError: Not enough arguments';
+    }
+    var padchar = base64.PADCHAR;
+    var alpha = base64.ALPHA;
+    var getbyte = base64.getbyte;
+
+    var i, b10;
+    var x = [];
+
+    // convert to string
+    s = '' + s;
+
+    var imax = s.length - (s.length % 3);
+
+    if (s.length == 0) {
+        return s;
+    }
+    for (i = 0; i < imax; i += 3) {
+        b10 = (getbyte(s, i) << 16) | (getbyte(s, i + 1) << 8) | getbyte(s, i + 2);
+        x.push(alpha.charAt(b10 >> 18));
+        x.push(alpha.charAt((b10 >> 12) & 0x3f));
+        x.push(alpha.charAt((b10 >> 6) & 0x3f));
+        x.push(alpha.charAt(b10 & 0x3f));
+    }
+    switch (s.length - imax) {
+        case 1:
+            b10 = getbyte(s, i) << 16;
+            x.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3f) + padchar + padchar);
+            break;
+        case 2:
+            b10 = (getbyte(s, i) << 16) | (getbyte(s, i + 1) << 8);
+            x.push(
+                alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3f) + alpha.charAt((b10 >> 6) & 0x3f) + padchar,
+            );
+            break;
+    }
+    return x.join('');
 };
